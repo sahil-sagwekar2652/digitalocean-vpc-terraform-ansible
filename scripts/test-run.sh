1aaf271145e9f2dc75fa63fc853283f9a372b3ed
sahil-sagwekar2652 <sahilss2652@gmail.com>
2023-10-08 20:44:12 +0530

    fix test-run.sh

--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -1,4 +1,5 @@
 echo "Press Ctrl+C to exit the loop\n"
+ip=$(cat ansible/output.json | jq '.loadbalancer_ip.value' | tr -d '"')
 
-for i in {1..10}; do curl $(cat ../ansible/output.json | jq '.loadbalancer_ip.value'); done
+for i in {1..10}; do curl $ip; done
 
